cbbd128170400ecc51a7455321e563f29f85b410
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-03-02 20:30:00 +0000

    Improved data reader.

--- trunk/src/treedata.cpp
+++ trunk/src/treedata.cpp
@@ -8,7 +8,13 @@
 using namespace std;
 
 Treedata::Treedata(string fname, bool is_featurerows):
-  targetidx_(-1)
+  targetidx_(-1),
+  nsamples_(0),
+  nfeatures_(0),
+  ncatfeatures_(0),
+  nnumfeatures_(0),
+  catfeatureics_(0),
+  numfeatureics_(0)
 {
 
   cout << "Treedata: reading matrix from file '" << fname << "'" << endl;
@@ -47,11 +53,65 @@ Treedata::Treedata(string fname, bool is_featurerows):
   ss.clear();
   ss.str("");
 
-  cout << ncols << " columns and " << nrows << " rows read." << endl;
+  vector<string> colheaders(ncols);
+  vector<string> rowheaders(nrows);
+  vector<vector<string> > datamatrix(nrows);
+  for(size_t i = 0; i < nrows; ++i)
+    {
+      datamatrix[i] = colheaders;
+    }
 
+  cout << "read " << datamatrix.size() << " rows and " << datamatrix[0].size() << " columns." << endl;
+
+  getline(featurestream,row);
+  ss << row;
+
+  for(size_t i = 0; i < ncols; ++i)
+    {
+      getline(ss,colheaders[i],'\t');
+      cout << '\t' << colheaders[i];
+    }
+  cout << endl;
+
+  for(size_t i = 0; i < nrows; ++i)
+    {
+      getline(featurestream,row);
+      ss.clear();
+      ss.str("");
+      ss << row;
+      getline(ss,rowheaders[i],'\t');
+      cout << rowheaders[i];
+      for(size_t j = 0; j < ncols; ++j)
+	{
+	  getline(ss,datamatrix[i][j],'\t');
+	  cout << '\t' << datamatrix[i][j];
+	}
+      cout << endl;
+    }
+  cout << endl;
+  
   if(is_featurerows)
     {
-      this->transpose();
+      nfeatures_ = nrows;
+      nsamples_ = ncols;
+      featureheaders_ = rowheaders;
+      for(size_t i = 0; i < nfeatures_; ++i)
+	{
+	  if(featureheaders_[0][0] == 'N')
+	    {
+	      numfeatureics_.push_back(i);
+	      ++nnumfeatures_;
+	    }
+	  else
+	    {
+	      catfeatureics_.push_back(i);
+	      ++ncatfeatures_;
+	    }
+	}
+    }
+  else
+    {
+      cerr << "samples as rows not yet supported!" << endl;
     }
 }
 
