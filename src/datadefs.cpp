a92a66fc753442446beeea21ee58d2de841f1999
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-03-03 07:36:18 +0000

    Added some helper functions for reading data (implementations mostly missing).

--- src/datadefs.cpp
+++ src/datadefs.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
-datadefs::cat_t datadefs::cat_nan = -1;
-datadefs::num_t datadefs::num_nan = sqrt(-1.0);
+const datadefs::cat_t datadefs::cat_nan = -1;
+const datadefs::num_t datadefs::num_nan = sqrt(-1.0);
 
-string initNA[] = {"NA","NAN"};
-set<string> datadefs::NANs(initNA,initNA+2);
-//string datadefs::NAN("NA");
+const string initNANs[] = {"NA","NAN"};
+const set<datadefs::NAN_t> datadefs::NANs(initNANs,initNANs+2);
+
+void datadefs::str2cat(vector<string>& strvec, vector<datadefs::cat_t>& catvec)
+{
+  
+}
+
+void datadefs::str2num(vector<string>& strvec, vector<datadefs::num_t>& numvec)
+{
+
+}
 
 datadefs::num_t datadefs::cat2num(datadefs::cat_t value)
 {
