e4addf107262bbcf3039e02a7a1cff7935e1f92b
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-03-03 07:36:18 +0000

    Added some helper functions for reading data (implementations mostly missing).

--- trunk/src/datadefs.hpp
+++ trunk/src/datadefs.hpp
@@ -11,16 +11,23 @@ using namespace std;
 namespace datadefs
 {
   
+  //Categorical data type
   typedef int cat_t;
+  //Numerical data type
   typedef float num_t;
 
-  extern cat_t cat_nan;
-  extern num_t num_nan;
+  //NaNs as represented by the program
+  extern const cat_t cat_nan;
+  extern const num_t num_nan;
 
-  //typedef string NAN_t;
-  extern set<string> NANs;
-  //extern string NAN;
+  //NaNs supported in the delimited file 
+  typedef string NAN_t;
+  extern const set<NAN_t> NANs;
 
+  void str2cat(vector<string>& strvec, vector<cat_t>& catvec);
+  void str2num(vector<string>& strvec, vector<num_t>& numvec);
+
+  //Function to convert categorical data to numerical
   num_t cat2num(cat_t value);
 
   bool is_nan(cat_t value);
