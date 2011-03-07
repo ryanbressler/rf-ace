a3fdaefbfe02228bb7f0243b02c1c6090e2978e3
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-03-07 14:05:37 +0000

    Included computation of an upper bound forest in Randomforest class. Memory will be reserved for that upper bound forest.

--- src/randomforest.cpp
+++ src/randomforest.cpp
@@ -1,4 +1,6 @@
 #include "randomforest.hpp"
+#include<cmath>
+#include<iostream>
 
 Randomforest::Randomforest(Treedata* treedata, size_t ntrees, size_t mtry, size_t nodesize):
   treedata_(treedata),
@@ -6,7 +8,27 @@ Randomforest::Randomforest(Treedata* treedata, size_t ntrees, size_t mtry, size_
   mtry_(mtry),
   nodesize_(nodesize)
 {
+
+  //First we count the theoretical maximum number of nodes per tree.
+  //Because each leaf must contain at least nodesize amount of data points, nmaxleaves is
+  int nmaxleaves = int(ceil(float(treedata_->nsamples())/nodesize_));
+  //The upper bound for depth of the tree is log2(nmaxleaves)=log10(nmaxleaves)/log10(2.0):
+  int maxdepth = int(ceil(log10(float(nmaxleaves))/log10(2.0)));
+  //Thus, the number of nodes in a complete binary tree of depth maxdepth, there are
+  int nmaxnodes = int(pow(2.0,maxdepth+1)); //In reality it's nmaxnodes-1 but this way we'll get a power of two which is supposed to be faster :)
   
+  //Reserve memory for one tree
+  vector<Node> tree(nmaxnodes);
+
+  //Reserve memory for the whole forest
+  vector<vector<Node> > forest(ntrees_);
+  treemap_ = forest;
+  for(size_t i = 0; i < ntrees_; ++i)
+    {
+      treemap_[i] = tree;
+    }
+
+  cout << "Forest initialized. " << treemap_.size() << " trees and " << nmaxnodes << " max nodes per tree generated." << endl;
 }
 
 Randomforest::~Randomforest()
