d9996a40a640ecba9f3ce69b7a40a90f8e762ef7
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-03-10 15:46:38 +0000

    Added comments to the Randomforest header.

--- src/randomforest.hpp
+++ src/randomforest.hpp
@@ -11,23 +11,36 @@ class Randomforest
 {
 public:
 
-  //Grows a Random Forest with ntrees, mtry, and nodesize
+  //Initializes Random Forest with ntrees, mtry, and nodesize
+  //NOTE: target feature will be set to 0. Change with Randomforest::select_target()
   Randomforest(Treedata* treedata, size_t ntrees, size_t mtry, size_t nodesize);
   ~Randomforest();
 
+  //Selects the target feature that is to be predicted
   void select_target(size_t targetidx);
+
+  //Gets the selected target feature
   size_t get_target();
 
+  //Grow the Random Forest with respect to selected target feature
   void grow_forest();
+
+  //Grow the Random Forest with respect to targetidx
   void grow_forest(size_t targetidx);
 
 private:
 
+  //Grows one tree with respect to selected target feature
   void grow_tree(size_t treeidx);
+
+  //Recursive tree-generating node splitter algorithm
+  //NOTE: there will be at least two alternative node splitter algorithms in the future
   void recursive_nodesplit(size_t treeidx, size_t nodeidx, vector<size_t> const& sampleics);
 
+  //Pointer to treedata_ object, stores all the feature information
   Treedata* treedata_;
   
+  //Size of the forest
   size_t ntrees_;
   size_t mtry_;
   size_t nodesize_;
