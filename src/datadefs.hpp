5ce046a0060b9d66be39f562d7aabd162b826620
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-04-03 11:02:44 +0000

    Updated handling of missing values. Implemented an interative formula for gini impurity. Killed some bugs and optimized code.

--- src/datadefs.hpp
+++ src/datadefs.hpp
@@ -8,6 +8,7 @@
 #include<math.h>
 #include<map>
 #include<cassert>
+#include<iostream>
 
 using namespace std;
 
@@ -26,72 +27,97 @@ namespace datadefs
   typedef string NAN_t;
   extern const set<NAN_t> NANs;
 
-  void strv2catv(vector<string>& strvec, vector<num_t>& catvec, map<string,size_t>& str2valmap);
+  void strv2catv(vector<string>& strvec, vector<num_t>& catvec);
   void strv2numv(vector<string>& strvec, vector<num_t>& numvec);
 
   num_t str2num(string& str);
 
-  bool is_nan(string& str);
-  bool is_nan(num_t value);
+  bool is_nan(const string& str);
+  bool is_nan(const num_t value);
   
   void sqerr(vector<num_t> const& data, 
 	     num_t& mu, 
 	     num_t& se,
 	     size_t& nreal);      
-
-  /*
-    void twoway_update_sqerr(const datadefs::num_t x_n,
-    size_t& n_left,
-    datadefs::num_t& mu_left,
-    datadefs::num_t& se_left,
-    size_t& n_right,
-    datadefs::num_t& mu_right,
-    datadefs::num_t& se_right);
-  */
   
+  //void quicksort(vector<num_t>& data);
+
+  void count_real_values(vector<num_t> const& data, size_t& nreal);
+
   void update_sqerr(const num_t x_n,
-		    const size_t n_left,
+		    size_t& n_left,
 		    num_t& mu_left,
 		    num_t& se_left,
-		    const size_t n_right,
+		    size_t& n_right,
 		    num_t& mu_right,
 		    num_t& se_right);
 
-  void count_freq(vector<num_t>& data, map<num_t,size_t>& cat2freq);
+  void count_freq(vector<num_t> const& data, map<num_t,size_t>& cat2freq, size_t& nreal);
 
   void map_data(vector<num_t>& data, 
-		map<num_t,vector<size_t> >& datamap);
+		map<num_t,vector<size_t> >& datamap,
+		size_t& nreal);
 
   void gini(vector<num_t>& data,
-	    num_t& gi);
+	    num_t& gi,
+	    size_t& nreal);
 
   void gini(map<num_t,size_t>& cat2freq,
             num_t& gi);
   
-  void update_gini(num_t x_n,
-		   const size_t n_left,
-		   map<num_t,size_t>& cat2freq_left,
-		   num_t& gi_left,
-		   const size_t n_right,
-		   map<num_t,size_t>& cat2freq_right,
-		   num_t& gi_right);
+  /*
+    void update_gini(num_t x_n,
+    const size_t n_left,
+    map<num_t,size_t>& cat2freq_left,
+    num_t& gi_left,
+    const size_t n_right,
+    map<num_t,size_t>& cat2freq_right,
+    num_t& gi_right);
+  */
+
+  void sqfreq(vector<num_t> const& data, 
+	      map<num_t,size_t>& freq, 
+	      num_t& sf, 
+	      size_t& nreal);
+
+  void update_sqfreq(const num_t x_n,
+		     size_t& n_left,
+		     map<num_t,size_t>& freq_left,
+		     num_t& sf_left,
+		     size_t& n_right,
+		     map<num_t,size_t>& freq_right,
+		     num_t& sf_right);
 
   void range(vector<size_t>& ics);
 
   //A comparator functor that can be passed to STL::sort. Assumes that one is comparing first elements of pairs, first type being num_t and second T
+  
   template <typename T> struct ordering {
     bool operator ()(pair<datadefs::num_t,T> const& a, pair<datadefs::num_t,T> const& b)
     {
-      if(a.first < b.first || b.first != b.first)
-	{
-	  return(true);
-	}
-      else
-	{
-	  return(false);
-	}      
+      return(a.first < b.first);
     }
   }; 
+  
+  /*
+    template <typename T> struct ordering {
+    bool operator ()(pair<datadefs::num_t,T> const& a, pair<datadefs::num_t,T> const& b)
+    {
+    //cout << a.first << " < " << b.first << "(" << a.second << "," << b.second << ")" ;
+    if(a.first < b.first || (a.first == a.first && b.first != b.first))
+    {
+    //cout << "=> true" << endl;
+    return(true);
+    }
+    else
+    {
+    //cout << "=> false" << endl;
+    return(false);
+    }
+    }
+    };
+  */
+  
 
   template <typename T1,typename T2> void make_pairedv(vector<T1> const& v1,
                                                        vector<T2> const& v2,
@@ -119,11 +145,15 @@ namespace datadefs
 
   template <typename T> void sort_and_make_ref(vector<T>& v, vector<size_t>& ref_ics)
   {
+    //cout << "sort_and_make_ref: in the beginning" << endl;
     assert(v.size() == ref_ics.size());
     vector<pair<T,size_t> > pairedv(ref_ics.size());
     datadefs::range(ref_ics);
+    //cout << "sort_and_make_ref: used range()" << endl;
     datadefs::make_pairedv<T,size_t>(v,ref_ics,pairedv);
+    //cout << "sort_and_make_ref: made pairedv" << endl;
     sort(pairedv.begin(),pairedv.end(),datadefs::ordering<T>());
+    //cout << "sort_and_make_ref: pairedv sorted" << endl;
     datadefs::separate_pairedv<T,size_t>(pairedv,v,ref_ics);
   }
 
