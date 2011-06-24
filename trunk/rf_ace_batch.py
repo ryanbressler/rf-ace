c890f629f94d56ea50bbb3b8d653012f4f6600a5
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-06-24 20:08:49 +0000

    Modified rf_ace_batch.py.

--- trunk/rf_ace_batch.py
+++ trunk/rf_ace_batch.py
@@ -6,7 +6,7 @@ assert int(sys.argv[2]) <= int(sys.argv[3])
 xlist = xrange(int(sys.argv[2]),int(sys.argv[3])+1)
 
 for targetidx in xlist:
-    os.system('bin/rf_ace --input '+sys.argv[1]+' --targetidx '+str(targetidx)+' --output '+sys.argv[4]+'_'+str(targetidx)+'.out')
+    os.system('bin/rf_ace --input '+sys.argv[1]+' --targetidx '+str(targetidx)+' --output '+sys.argv[4]+'_'+str(targetidx))
 
-os.system('cat '+sys.argv[4]+'_*.out > '+sys.argv[4]+'.out')
-os.system('rm '+sys.argv[4]+'_*.out')
+os.system('cat '+sys.argv[4]+'_* > '+sys.argv[4])
+os.system('rm '+sys.argv[4]+'_*')
