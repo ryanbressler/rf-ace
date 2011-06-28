207ef82a186699d40e2c22f68e432a7e0e1a7991
kari.torkkola@gmail.com <kari.torkkola@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-06-28 07:19:26 +0000

    Implemented GBT for a categorigal target (K-class logistic gradient boosting).

--- trunk/src/GBT.cpp
+++ trunk/src/GBT.cpp
@@ -9,35 +9,9 @@ nMaxLeaves_(nMaxLeaves),
 shrinkage_(shrinkage),
 subSampleSize_(subSampleSize)
 {
-	// Determine the number of nodes we need for the GBT-forest.
-	//
-	// nMaxLeaves determines the max number of leaf nodes
-	// The depth of the last complete level of the tree:
-	int depthComplete = int(floor(log10(float(nMaxLeaves))/log10(2.0)));
-	// Number of nodes of the last complete level of the tree:
-	int nTerminalNodesComplete = int(pow(2.0, depthComplete));
-	//
-	// The number of nodes in a complete binary tree of depth depthComplete
-	// plus the last incomplete level
-	nMaxNodes_ = 2*nTerminalNodesComplete-1  + 2*( nMaxLeaves-nTerminalNodesComplete );
-
-	// Reserve memory for vector of node counts in each tree
-	nNodes_ = vector<size_t>(nTrees_);
-	// Reserve memory for the forest "pointer" vector
-	forest_ = vector< vector<Node> >(nTrees_);
-
-	// Memory for nodes of each individual tree
-	for(size_t t = 0; t < nTrees_; ++t)
-	{
-		forest_[t] = vector<Node>(nMaxNodes_);
-		nNodes_[t] = 0;
-	}
-
-	cout << "Forest initialized. " << forest_.size() << " trees and "
-			<< nMaxNodes_ << " max nodes per tree generated." << endl;
-
-	// Currently, we grow the forest outside of the constructor
-	// GBT::growForest();
+	numClasses_ = 0; // Denotes regression forest by default
+	
+	// Currently, we allocate and grow the forest outside of the constructor
 }
 
 
@@ -48,23 +22,49 @@ GBT::~GBT()
 }
 
 
-
-// Predict using a GBT "forest" from an arbitrary data set
-void GBT::predictForest(Treedata* treeData, vector<num_t>& prediction)
+// reserve memory
+void GBT::allocateForest()
 {
-	size_t nSamples = treeData->nsamples();
-	size_t iTarget  = treeData->get_target();
-	cout << "Predicting "<<nSamples<<" samples. Target="<<iTarget<<endl;
-	for (size_t i=0; i<nSamples; i++)
+	// Determine the number of nodes we need for the GBT-forest.
+	//
+	// nMaxLeaves determines the max number of leaf nodes
+	// The depth of the last complete level of the tree:
+	int depthComplete = int(floor(log10(float(nMaxLeaves_))/log10(2.0)));
+	// Number of nodes of the last complete level of the tree:
+	int nTerminalNodesComplete = int(pow(2.0, depthComplete));
+	//
+	// The number of nodes in a complete binary tree of depth depthComplete
+	// plus the last incomplete level
+	nMaxNodes_ = 2*nTerminalNodesComplete-1  + 2*( nMaxLeaves_-nTerminalNodesComplete );
+
+	// for classification, we need the number of classes, 
+	// because it will multiply the number of trees
+	size_t iTarget = GBT::getTarget();
+	if ( !treeData_->isfeaturenum( iTarget ) )
 	{
-		prediction[i] = 0;
-		for(size_t t = 0; t < nTrees_; ++t)
-		{
-			prediction[i] = prediction[i] + shrinkage_ * predictSampleByTree(i, t, treeData);
-		}
-		// print out the true and the prediction
-		cout << i << "\t" << treeData->featurematrix_[iTarget][i] << "\t" << prediction[i] <<endl;
+		map<num_t,size_t> cat2freq;
+		size_t nReal;
+		datadefs::count_freq( treeData_->featurematrix_[iTarget], cat2freq, nReal);
+		numClasses_ = cat2freq.size(); // set numClasses_ to denote a classification forest
+		cout << "Target "<<iTarget<<" has "<<numClasses_<<" classes, "<<nReal<<" non-missing."<<endl;
+		nTrees_ = nTrees_ * numClasses_;
+		cout << "Target "<<iTarget<<" has "<<numClasses_<<" classes, "<<nReal<<" non-missing."<<endl;
 	}
+
+	// Reserve memory for vector of node counts in each tree
+	nNodes_ = vector<size_t>(nTrees_);
+	// Reserve memory for the forest "pointer" vector
+	forest_ = vector< vector<Node> >(nTrees_);
+	
+	// Memory for nodes of each individual tree
+	for(size_t t = 0; t < nTrees_; ++t)
+	{
+		forest_[t] = vector<Node>(nMaxNodes_);
+		nNodes_[t] = 0;
+	}
+
+	cout << "Forest initialized. " << forest_.size() << " trees and "
+			<< nMaxNodes_ << " max nodes per tree generated." << endl;
 }
 
 
@@ -72,6 +72,21 @@ void GBT::predictForest(Treedata* treeData, vector<num_t>& prediction)
 
 // Grow a GBT "forest"
 void GBT::growForest()
+{
+	if ( treeData_->isfeaturenum( GBT::getTarget() ) )
+	{
+		GBT::growForestNumerical();
+	}
+	else
+	{
+		GBT::growForestCategorical();
+	}
+}
+
+
+
+// Grow a GBT "forest" for a numerical target variable
+void GBT::growForestNumerical()
 {
 	//---------------------------------------------------------------------------
 	// The target attribute in treedata should have been set beforehand
@@ -107,7 +122,6 @@ void GBT::growForest()
 		// set target in order to re-sort
 		//treeData_->select_target( iTarget );
 		treeData_->targetidx_ = iTarget;
-		// TODO check if sorting might mess up things at some later point
 
 		// Grow a tree to predict the current target
 		GBT::growTree(t);
@@ -125,7 +139,192 @@ void GBT::growForest()
 
 	// restore true target column
 	treeData_->featurematrix_[iTarget] = trueTarget;
-	// TODO check if sample order is now totally wrong?
+}
+
+
+// Grow a GBT "forest" for a categorical target variable
+void GBT::growForestCategorical()
+{
+	//---------------------------------------------------------------------------
+	// The target attribute in treedata should have been set beforehand
+	size_t iTarget  = treeData_->get_target();
+	size_t nSamples = treeData_->nsamples();
+	
+	// Save a copy of the target column because it will be overwritten later.
+	// We also know that it must be categorical.
+	vector<num_t> trueTarget = treeData_->featurematrix_[iTarget];
+
+	// Target for GBT is different for each tree.
+	// We use the original target column to save each temporary target.
+	// Reference to the target column, will overwrite it:
+	vector<num_t>& curTarget = treeData_->featurematrix_[iTarget];
+
+	// For categorical target variables, we predict each category probability separately.
+	// This target is numerical, thus we need to change the target variable type to 
+	// numerical from categorical.
+	treeData_->isfeaturenum_[iTarget] = true;
+	
+	
+	// Initialize class probability estimates and the predictions.
+	// Note that dimensions in these two are reversed!
+	vector< vector<num_t> > prediction(    nSamples, vector<num_t>( numClasses_, 0.0 ) );
+	vector< vector<num_t> > curPrediction( numClasses_, vector<num_t>( nSamples, 0.0 ) );
+	
+	vector< vector<num_t> > curProbability( nSamples, vector<num_t>( numClasses_)  );
+	// TODO don't really need all this space allocated
+		
+	
+	// Each iteration consists of numClasses_ trees, 
+	// each of those predicting the probability residual for each class.
+	size_t numIterations = nTrees_ / numClasses_;
+	
+	for(size_t m=0; m < numIterations; ++m)
+	{	
+		// Multiclass logistic transform of class probabilities from current probability estimates.
+		for (size_t i=0; i<nSamples; i++)
+		{		
+			GBT::transformLogistic( prediction[i],  curProbability[i]); 
+			// each prediction[i] is a vector<num_t>(numClasses_)
+		}		
+		
+		// construct a tree for each class
+		for (size_t k = 0; k < numClasses_; ++k)
+		{
+			// target for class k is ...
+			for (size_t i=0; i<nSamples; i++)
+			{
+				// ... the difference between true target and current prediction
+				curTarget[i] = (k==trueTarget[i]) - curProbability[i][k];
+			}
+
+			// Grow a tree to predict the current target
+			size_t t = m*numClasses_ + k; // tree index
+			GBT::growTree(t);
+
+			// What kind of a prediction does the new tree produce?
+			GBT::predictDatasetByTree(t, curPrediction[k] );
+
+			// Calculate the current total prediction adding the newly generated tree
+			for (size_t i=0; i<nSamples; i++)
+			{
+				prediction[i][k] = prediction[i][k] + shrinkage_ * curPrediction[k][i];
+			}
+		}
+	}
+		
+	// GBT-forest is now done!
+	// restore the true target column and its true type
+	treeData_->featurematrix_[iTarget] = trueTarget;
+	treeData_->isfeaturenum_[iTarget] = false;
+}
+
+
+
+void GBT::transformLogistic(vector<num_t>& prediction, vector<num_t>& probability)
+{
+	// Multiclass logistic transform of class probabilities from current probability estimates.
+	assert( numClasses_ == prediction.size() );	
+	vector<num_t>& expPrediction = probability; // just using the space by a different name
+	
+	// find maximum prediction
+	vector<num_t>::iterator maxPrediction = max_element( prediction.begin(), prediction.end() );
+	// scale by maximum to prevent numerical errors
+	
+
+
+	num_t expSum = 0.0;
+	size_t k;
+	for(k=0; k < numClasses_; ++k)
+	{
+		expPrediction[k] = exp( prediction[k] - *maxPrediction ); // scale by maximum
+		expSum += expPrediction[k];		
+	}
+	for(k = 0; k < numClasses_; ++k)
+	{
+		probability[k] = expPrediction[k] / expSum;	
+	}			
+}
+
+
+
+// Predict using a GBT "forest" from an arbitrary data set. GBT::numClasses_ determines 
+// whether the prediction is for a categorical or a numerical variable.
+void GBT::predictForest(Treedata* treeData, vector<num_t>& prediction)
+{
+	if ( 0 == numClasses_ ) 
+	{
+		predictForestNumerical(treeData, prediction);
+	} 
+	else 
+	{
+		predictForestCategorical(treeData, prediction);
+	}
+	
+}
+
+
+// Predict categorical target using a GBT "forest" from an arbitrary data set. 
+void GBT::predictForestCategorical(Treedata* treeData, vector<num_t>& categoryPrediction)
+{
+	size_t nSamples = treeData->nsamples();
+	size_t iTarget  = treeData->get_target();
+	cout << "Predicting "<<nSamples<<" samples. Target="<<iTarget<<". Classes="<<numClasses_<<endl;
+	
+	// for classification, each "tree" is actually numClasses_ trees in a row
+	// each predicting the probability of its own class.
+	size_t numIterations = nTrees_ / numClasses_;
+	size_t errorCnt = 0;
+	vector<num_t> probPrediction( numClasses_ );
+
+	
+	for (size_t i=0; i<nSamples; i++)
+	{
+		vector<num_t> prediction( numClasses_ , 0);
+		for (size_t k=0; k<numClasses_; ++k) // 
+		{
+			for(size_t m = 0; m < numIterations; ++m)
+			{
+				size_t t =  m*numClasses_ + k; // tree index
+				prediction[k] = prediction[k] + shrinkage_ * predictSampleByTree(i, t, treeData);
+			}
+		}
+		// find index of maximum prediction, this is the predicted cateory
+		vector<num_t>::iterator maxProb = max_element( prediction.begin(), prediction.end() );
+		categoryPrediction[i] = maxProb - prediction.begin() ; // classes are 0,1,2,...
+		
+		
+		
+		// diagnostic print out the true and the prediction
+		errorCnt += (treeData->featurematrix_[iTarget][i] != categoryPrediction[i]);
+		cout << i << "\t" << treeData->featurematrix_[iTarget][i] << "\t" << categoryPrediction[i];
+		GBT::transformLogistic(prediction, probPrediction); // predictions-to-probabilities
+		for (size_t k=0; k<numClasses_; ++k)
+		{
+			cout <<"\t"<<probPrediction[k];
+		}
+		cout <<endl;
+	}
+	cout<<"Error "<<errorCnt<<"/"<<nSamples<<"="<<100*errorCnt/nSamples<<"%"<<endl;
+}
+
+
+
+// Predict numerical target using a GBT "forest" from an arbitrary data set
+void GBT::predictForestNumerical(Treedata* treeData, vector<num_t>& prediction)
+{
+	size_t nSamples = treeData->nsamples();
+	size_t iTarget  = treeData->get_target();
+	cout << "Predicting "<<nSamples<<" samples. Target="<<iTarget<<endl;
+	for (size_t i=0; i<nSamples; i++)
+	{
+		prediction[i] = 0;
+		for(size_t t = 0; t < nTrees_; ++t)
+		{
+			prediction[i] = prediction[i] + shrinkage_ * predictSampleByTree(i, t, treeData);
+		}
+		// diagnostic print out the true and the prediction
+		cout << i << "\t" << treeData->featurematrix_[iTarget][i] << "\t" << prediction[i] <<endl;
+	}
 }
 
 
@@ -198,12 +397,38 @@ void GBT::SetNodePrediction( size_t treeidx, size_t nodeidx, vector<size_t>& sam
 	{
 		data[i] = (treeData_->featurematrix_)[targetidx][ sampleics[i] ];
 	}
-	num_t mu,se;
-	size_t nreal;
-	datadefs::sqerr( data, mu,se,nreal); // only need mu
-
-	forest_[treeidx][nodeidx].set_prediction(mu);
-	cout << "setting pred=" << mu << ", tree="<<treeidx<<" node="<<nodeidx<<endl;
+	
+	num_t nodePred;
+	if (0==numClasses_) 
+	{
+		// numerical target
+		num_t se;
+		size_t nreal;
+		datadefs::sqerr( data, nodePred,se,nreal); // only need mu as the node Pred
+		// TODO we do no need everything that the above calculates
+	}
+	else
+	{
+		// categorical target, we are predicting one class probability, calculate gamma
+		num_t numerator=0.0, denominator=0.0;
+		for(size_t i = 0; i < data.size(); ++i)
+		{
+			num_t abs_data_i = abs( data[i] );
+			denominator += abs_data_i * (1.0 - abs_data_i);
+			numerator   += data[i];
+		}
+		if ( abs(denominator) <= datadefs::eps )
+		{
+			nodePred = LOG_OF_MAX_FLT * numerator;
+		}
+		else
+		{
+			nodePred = (numClasses_ - 1)*numerator / (numClasses_ * denominator);
+		}
+	}
+		
+	forest_[treeidx][nodeidx].set_prediction(nodePred);
+	cout << "setting pred=" << nodePred << ", tree="<<treeidx<<" node="<<nodeidx<<endl;
 }
 
 
--- trunk/src/GBT.hpp
+++ trunk/src/GBT.hpp
@@ -2,8 +2,13 @@
 #define GBT_HPP
 
 #include <cstdlib>
+#include <algorithm>
 #include "node.hpp"
 #include "treedata.hpp"
+#include "datadefs.hpp"
+
+#define LOG_OF_MAX_FLT 70.0f
+
 
 using namespace std;
 
@@ -18,6 +23,8 @@ public:
 	GBT(Treedata* treeData, size_t nTrees, size_t nMaxLeaves, float shrinkage, float subSampleSize);
 	~GBT();
 
+	// memory for the forest
+	void allocateForest();
 	// actual growing
 	void growForest();
 	// produce predictions of a data set
@@ -30,11 +37,18 @@ public:
 
 private:
 
+	void growForestNumerical();
+	void growForestCategorical();
 	void growTree(size_t treeidx);
+	
+	void predictForestNumerical(  Treedata* treeData, vector<num_t>& prediction);
+	void predictForestCategorical(Treedata* treeData, vector<num_t>& categoryPrediction);
 	num_t predictSampleByTree(size_t sampleidx, size_t treeidx, Treedata* treeData);
 	void predictDatasetByTree(size_t treeidx, vector<num_t>& prediction);
+	
 	void recursiveNodesplit(size_t treeidx, size_t nodeidx, vector<size_t>& sampleics);
 	void SetNodePrediction( size_t treeidx, size_t nodeidx, vector<size_t>& sampleics);
+	void transformLogistic(vector<num_t>& prediction, vector<num_t>& probability);
 
 	Treedata* treeData_;
 	size_t nTrees_;
@@ -42,6 +56,7 @@ private:
 	size_t nMaxNodes_;
 	float shrinkage_;
 	float subSampleSize_;
+	size_t numClasses_;
 
 	vector<size_t> nNodes_; //Number of used nodes in each tree.
 	vector<vector<Node> > forest_; // forest_[i][j] is the j'th node of i'th tree. forest_[i][0] is the rootnode.
