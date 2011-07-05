888058c920bda9c140803f8d99b02ae66973b70d
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-07-05 19:50:14 +0000

    Implemented a feature that allows one to feed in substrings that match one or more feature headers in the input file, and use all the matching ones as targets, one by one, and catenate associations to the specified output file.

--- data/scriptTestDataGenerator.m
+++ data/scriptTestDataGenerator.m
@@ -4,26 +4,26 @@
 clear par;
 
 % basic parameters
-par.dependency = 'linear'; % or 'nonlinear'
-%par.dependency = 'nonlinear'; 
+%par.dependency = 'linear'; % or 'nonlinear'
+par.dependency = 'nonlinear'; 
 par.O = 1;      % number of target variables 
 par.N = 200;   % number of samples generated
-par.n = 10;     % number of relevant variables from which target is generated
-par.seed = 1;   % random number generator seed, if not given, generated from time
-par.sets = 1;   % how many data sets to generate
+par.n = 5;     % number of relevant variables from which target is generated
+%par.seed = 1;   % random number generator seed, if not given, generated from time
+par.sets = 10;   % how many data sets to generate
 par.testFraction = 0; % fraction of each set written to test file
 
 % used by nonlinear dependency generation
-par.L = 10;     % number of functions added together to construct the target
+par.L = 5;     % number of functions added together to construct the target
 
 % used for linear dependency generation, if not specified, will generate randomly 
 par.P = 1:(-0.1):0.1;
 %par.P = [1 0.5 0.25 0.125 0.0625];
 
 % post dependency generation
-par.Kn = 20000;    % number of additional noise variables concatenated to data
+par.Kn = 100;    % number of additional noise variables concatenated to data
 par.maxClasses = 0;       % discretize target, 0=regression
-par.mixedType  = 0.1;     % discretize this fraction of the input variables
+par.mixedType  = 0.0;     % discretize this fraction of the input variables
 par.maxLevels = 6;       % max num discrete levels
 par.randomizeTarget=0.02; % add noise to target with var 'randomizeTarget'
 par.missing = 0.1;        % fraction of missing values
--- src/rf_ace.cpp
+++ src/rf_ace.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <ctime>
 #include <cmath>
 #include <stdio.h>
@@ -19,7 +20,7 @@ using namespace GetOpt;
 
 enum MatrixFormat { FEATURE_ROWS, FEATURE_COLUMNS };
 
-const int TARGETIDX_NOT_SET = -1;
+//const int TARGETSTR_NOT_SET = -1;
 const size_t DEFAULT_NTREES = 0;
 const size_t DEFAULT_MTRY = 0;
 const size_t DEFAULT_NODESIZE = 0;
@@ -27,15 +28,47 @@ const size_t DEFAULT_NPERMS = 50;
 const num_t DEFAULT_PVALUETHRESHOLD = 0.10;
 const int DEFAULT_IS_GBT_ENABLED = 0;
 
+bool isPositiveInteger(const string& str, size_t& positiveInteger)
+{
+  stringstream ss(str);
+  int testInt;
+  if(ss >> testInt && ss.eof() && testInt >= 0)
+    {
+      ss.clear();
+      ss.str("");
+      ss >> positiveInteger;
+      return(true);
+    }
+  else
+    {
+      return(false);
+    }
+}
+
+void getMatchingTargetIcs(Treedata& treedata, const string& targetStr, set<size_t>& targetIcs)
+{
+  targetIcs.clear();
+  for(size_t featureIdx = 0; featureIdx < treedata.nFeatures(); ++featureIdx)
+    {
+      string featureName(treedata.getFeatureName(featureIdx));
+      if( featureName.find(targetStr) != string::npos)
+	{
+	  targetIcs.insert(featureIdx);
+	}
+    }
+  
+}
+
 void printHeader()
 {
   cout << endl;
   cout << " --------------------------------------------------------------- " << endl;
   cout << "| RF-ACE -- efficient feature selection with heterogeneous data |" << endl;
   cout << "|                                                               |" << endl;
-  cout << "|  Version:      RF-ACE v0.5.2, July 4th, 2011                  |" << endl;
+  cout << "|  Version:      RF-ACE v0.5.4, July 4th, 2011                  |" << endl;
   cout << "|  Project page: http://code.google.com/p/rf-ace                |" << endl;
   cout << "|  Contact:      timo.p.erkkila@tut.fi                          |" << endl;
+  cout << "|                kari.torkkola@gmail.com                        |" << endl;
   cout << "|                                                               |" << endl;
   cout << "|              DEVELOPMENT VERSION, BUGS EXIST!                 |" << endl;
   cout << " --------------------------------------------------------------- " << endl;
@@ -46,7 +79,7 @@ void printHelp()
   cout << endl;
   cout << "REQUIRED ARGUMENTS:" << endl;
   cout << " -I / --input        input feature file (AFM or ARFF)" << endl;
-  cout << " -i / --targetidx    target index, reference to order of appearance in the input file" << endl;
+  cout << " -i / --target       target, specified as integer or string that is to be matched with the content of input" << endl;
   cout << " -O / --output       output association file" << endl;
   cout << endl;
   cout << "OPTIONAL ARGUMENTS:" << endl;
@@ -96,7 +129,7 @@ int main(int argc, char* argv[])
 
   //Set user parameters to default values
   string input = "";
-  int targetIdx = TARGETIDX_NOT_SET;
+  string targetStr = "";
   size_t nTrees = DEFAULT_NTREES;
   size_t mTry = DEFAULT_MTRY;
   size_t nodeSize = DEFAULT_NODESIZE;
@@ -108,7 +141,7 @@ int main(int argc, char* argv[])
   //Read the user parameters 
   GetOpt_pp ops(argc, argv);        
   ops >> Option('I',"input",input);
-  ops >> Option('i',"targetidx",targetIdx);
+  ops >> Option('i',"target",targetStr);
   ops >> Option('n',"ntrees",nTrees);
   ops >> Option('m',"mtry",mTry);
   ops >> Option('s',"nodesize",nodeSize);
@@ -126,9 +159,9 @@ int main(int argc, char* argv[])
     }
 
   //Print help and exit if target index is not specified
-  if(targetIdx == TARGETIDX_NOT_SET)
+  if(targetStr == "")
     {
-      cerr << "targetidx (-i/--targetidx) must be set" << endl;
+      cerr << "target(s) (-i/--target) not specified" << endl;
       printHelpHint();
       return EXIT_FAILURE;
     }
@@ -145,191 +178,223 @@ int main(int argc, char* argv[])
   cout << endl << "Reading file '" << input << "'" << endl;
   Treedata treedata(input);
 
-  //If the specified target index exceeds the dimensions of the treedata object, print help and exit
-  if(targetIdx < 0 || targetIdx >= static_cast<int>(treedata.nFeatures()))
+  size_t targetIdx = 0;
+  set<size_t> targetIcs;
+  if( isPositiveInteger(targetStr,targetIdx) )
     {
-      cerr << "targetidx must point to a valid feature (0.." << treedata.nFeatures()-1 << ")" << endl;
-      printHelpHint();
-      return EXIT_FAILURE;
-    }
-
-  size_t nRealSamples = treedata.nRealSamples(targetIdx);
-
-  //If the target has no real samples, the program will just exit
-  if(nRealSamples == 0)
-    {
-      cout << "Target has no real samples, quitting..." << endl;
-      return EXIT_SUCCESS;
-    }
-
-  num_t realFraction = 1.0*nRealSamples / treedata.nSamples();
-
-  //If default nTrees is to be used...
-  if(nTrees == DEFAULT_NTREES)
-    {
-      nTrees = static_cast<size_t>( 1.0*treedata.nSamples() / realFraction );
-    }
-  
-  //If default mTry is to be used...
-  if(mTry == DEFAULT_MTRY)
-    {
-      mTry = static_cast<size_t>( floor( sqrt( 1.0*treedata.nFeatures() ) ) );   
-    }
-
-  //If default nodeSize is to be used...
-  if(nodeSize == DEFAULT_NODESIZE)
-    {
-      nodeSize = 5;
-      size_t altNodeSize = static_cast<size_t>( ceil( 1.0*nRealSamples/20 ) );
-      if(altNodeSize > nodeSize)
+      if( targetIdx >= treedata.nFeatures() )
 	{
-	  nodeSize = altNodeSize;
+	  cerr << "target must point to a valid feature (0.." << treedata.nFeatures()-1 << ")" << endl;
+	  printHelpHint();
+	  return EXIT_FAILURE;
 	}
-    }
-  
-  //Print values of parameters of RF-ACE
-  cout << endl;
-  cout << "RF-ACE parameter configuration:" << endl;
-  cout << "  --input      = " << input << endl;
-  cout << "  --nsamples   = " << nRealSamples << " / " << treedata.nSamples() << " (" << 100 * ( 1 - realFraction )<< "% missing)" << endl;
-  cout << "  --nfeatures  = " << treedata.nFeatures() << endl;
-  cout << "  --targetidx  = " << targetIdx << ", header '" << treedata.getFeatureName(targetIdx) << "'" << endl;
-  cout << "  --ntrees     = " << nTrees << endl;
-  cout << "  --mtry       = " << mTry << endl;
-  cout << "  --nodesize   = " << nodeSize << endl;
-  cout << "  --nperms     = " << nPerms << endl;
-  cout << "  --pthresold  = " << pValueThreshold << endl;
-  cout << "  --output     = " << output << endl << endl;
 
-  //Some assertions to check whether the parameter values and data dimensions allow construction of decision trees in the first place
-  //NOTE: consider writing a more sophisticated test and provide better print-outs than just assertion failures
-  assert(treedata.nFeatures() >= mTry);
-  assert(treedata.nSamples() > 2*nodeSize);
-
-  //////////////////////////////////////////////////////////////////////
-  //  ANALYSIS 1 -- Random Forest ensemble with artificial contrasts  //
-  //////////////////////////////////////////////////////////////////////
-
-  vector<num_t> pValues(treedata.nFeatures());
-  vector<num_t> importanceValues(treedata.nFeatures());
-  vector<vector<num_t> > importanceMat(nPerms);
-  size_t nNodesInAllForests = 0;
-
-  clock_t time_start(clock());
-
-  cout << "Growing " << nPerms << " Random Forests (RFs), please wait..." << endl;
-  for(size_t permIdx = 0; permIdx < nPerms; ++permIdx)
-    {
-      cout << "  RF " << permIdx + 1 << ": ";
-      Randomforest RF(&treedata,targetIdx,nTrees,mTry,nodeSize);
-      size_t nNodesInForest = RF.nNodes();
-      nNodesInAllForests += nNodesInForest;
-      importanceMat[permIdx] = RF.featureImportance();
-      cout << nNodesInForest << " nodes (avg. " << 1.0*nNodesInForest / nTrees << " nodes / tree)" << endl;
-    }
-
-  num_t time_diff = 1.0*(clock() - time_start) / CLOCKS_PER_SEC;
-  cout << nPerms << " RFs, " << nPerms*nTrees << " trees, and " << nNodesInAllForests
-       << " nodes generated in " << time_diff << " seconds (" << 1.0*nNodesInAllForests / time_diff
-       << " nodes per second)" << endl;
-
-  for(size_t featureIdx = 0; featureIdx < treedata.nFeatures(); ++featureIdx)
-    {
-
-      size_t nRealSamples;
-      vector<num_t> fSample(nPerms);
-      vector<num_t> cSample(nPerms);
-      for(size_t permIdx = 0; permIdx < nPerms; ++permIdx)
-        {
-          fSample[permIdx] = importanceMat[permIdx][featureIdx];
-          cSample[permIdx] = importanceMat[permIdx][featureIdx + treedata.nFeatures()];
-        }
-      datadefs::utest(fSample,cSample,pValues[featureIdx]);
-      datadefs::mean(fSample,importanceValues[featureIdx],nRealSamples);
-    }
-
-
-
-  /////////////////////////////////////////////
-  //  ANALYSIS 2 -- Gradient Boosting Trees  //
-  /////////////////////////////////////////////
-  //bool isGBTEnabled = false; //TODO: EXTEND THE USER PARAMETERS TO INCLUDE THIS
-  if(isGBTEnabled)
-    {
-      cout << "Gradient Boosting Trees *ENABLED*" << endl;
-      
-      //if(!treedata.isFeatureNumerical(targetIdx))
-      //	{
-      //	  nTrees *= treedata.nCategories(targetIdx);
-      //	}
-
-      size_t nMaxLeaves = 6;
-      num_t shrinkage = 0.5;
-      num_t subSampleSize = 0.5;
-      GBT gbt(&treedata, targetIdx, nTrees, nMaxLeaves, shrinkage, subSampleSize);
-      
-      
-      cout <<endl<< "PREDICTION:" << endl;
-      vector<num_t> prediction(treedata.nSamples());
-      gbt.predictForest(&treedata, prediction);
+      targetIcs.insert(targetIdx);
     }
   else
     {
-      cout << "Gradient Boosting Trees *DISABLED*" << endl;
+      getMatchingTargetIcs(treedata,targetStr,targetIcs);
     }
-  
 
-
-  ///////////////////////
-  //  GENERATE OUTPUT  //
-  ///////////////////////  
-  vector<size_t> refIcs(treedata.nFeatures());
-  //vector<string> fnames = treedata.featureheaders();
-  datadefs::sortDataAndMakeRef(pValues,refIcs);
-  datadefs::sortFromRef<num_t>(importanceValues,refIcs);
-  //targetIdx = refIcs[targetIdx];
-  
-  string targetName = treedata.getFeatureName(targetIdx);
-
-  if(pValues[0] <= pValueThreshold)
+  //If the specified target index exceeds the dimensions of the treedata object, print help and exit
+  /*
+    if(targetIdx < 0 || targetIdx >= static_cast<int>(treedata.nFeatures()))
     {
-      //cout << "Writing associations to file '" << output << "'" << endl;
-      //ofstream os(output.c_str());
-      FILE* po;
-      po = fopen(output.c_str(),"w");
+    cerr << "targetidx must point to a valid feature (0.." << treedata.nFeatures()-1 << ")" << endl;
+    printHelpHint();
+    return EXIT_FAILURE;
+    }
+  */
+
+  size_t iter = 1;
+  for(set<size_t>::const_iterator it(targetIcs.begin()); it != targetIcs.end(); ++it, ++iter)
+    {
+      targetIdx = *it;
+      
+      size_t nRealSamples = treedata.nRealSamples(targetIdx);
+      
+      //If the target has no real samples, the program will just exit
+      if(nRealSamples == 0)
+	{
+	  cout << "Target has no real samples, quitting..." << endl;
+	  return EXIT_SUCCESS;
+	}
+      
+      num_t realFraction = 1.0*nRealSamples / treedata.nSamples();
+      
+      //If default nTrees is to be used...
+      if(nTrees == DEFAULT_NTREES)
+	{
+	  nTrees = static_cast<size_t>( 1.0*treedata.nSamples() / realFraction );
+	}
+      
+      //If default mTry is to be used...
+      if(mTry == DEFAULT_MTRY)
+	{
+	  mTry = static_cast<size_t>( floor( sqrt( 1.0*treedata.nFeatures() ) ) );   
+	}
+      
+      //If default nodeSize is to be used...
+      if(nodeSize == DEFAULT_NODESIZE)
+	{
+	  nodeSize = 5;
+	  size_t altNodeSize = static_cast<size_t>( ceil( 1.0*nRealSamples/20 ) );
+	  if(altNodeSize > nodeSize)
+	    {
+	      nodeSize = altNodeSize;
+	    }
+	}
+      
+      //Print values of parameters of RF-ACE
+      cout << endl;
+      cout << "RF-ACE parameter configuration:" << endl;
+      cout << "  --input      = " << input << endl;
+      cout << "  --nsamples   = " << nRealSamples << " / " << treedata.nSamples() << " (" << 100 * ( 1 - realFraction )<< "% missing)" << endl;
+      cout << "  --nfeatures  = " << treedata.nFeatures() << endl;
+      cout << "  --targetidx  = " << targetIdx << ", header '" << treedata.getFeatureName(targetIdx) << "'" << endl;
+      cout << "  --ntrees     = " << nTrees << endl;
+      cout << "  --mtry       = " << mTry << endl;
+      cout << "  --nodesize   = " << nodeSize << endl;
+      cout << "  --nperms     = " << nPerms << endl;
+      cout << "  --pthresold  = " << pValueThreshold << endl;
+      cout << "  --output     = " << output << endl << endl;
+      
+      //Some assertions to check whether the parameter values and data dimensions allow construction of decision trees in the first place
+      //NOTE: consider writing a more sophisticated test and provide better print-outs than just assertion failures
+      assert(treedata.nFeatures() >= mTry);
+      assert(treedata.nSamples() > 2*nodeSize);
+      
+      //////////////////////////////////////////////////////////////////////
+      //  ANALYSIS 1 -- Random Forest ensemble with artificial contrasts  //
+      //////////////////////////////////////////////////////////////////////
+      
+      vector<num_t> pValues(treedata.nFeatures());
+      vector<num_t> importanceValues(treedata.nFeatures());
+      vector<vector<num_t> > importanceMat(nPerms);
+      size_t nNodesInAllForests = 0;
+      
+      clock_t time_start(clock());
+      
+      cout << "Growing " << nPerms << " Random Forests (RFs), please wait..." << endl;
+      for(size_t permIdx = 0; permIdx < nPerms; ++permIdx)
+	{
+	  cout << "  RF " << permIdx + 1 << ": ";
+	  Randomforest RF(&treedata,targetIdx,nTrees,mTry,nodeSize);
+	  size_t nNodesInForest = RF.nNodes();
+	  nNodesInAllForests += nNodesInForest;
+	  importanceMat[permIdx] = RF.featureImportance();
+	  cout << nNodesInForest << " nodes (avg. " << 1.0*nNodesInForest / nTrees << " nodes / tree)" << endl;
+	}
+      
+      num_t time_diff = 1.0*(clock() - time_start) / CLOCKS_PER_SEC;
+      cout << nPerms << " RFs, " << nPerms*nTrees << " trees, and " << nNodesInAllForests
+	   << " nodes generated in " << time_diff << " seconds (" << 1.0*nNodesInAllForests / time_diff
+	   << " nodes per second)" << endl;
       
       for(size_t featureIdx = 0; featureIdx < treedata.nFeatures(); ++featureIdx)
 	{
-
-	  if(pValues[featureIdx] > pValueThreshold)
+	  
+	  size_t nRealSamples;
+	  vector<num_t> fSample(nPerms);
+	  vector<num_t> cSample(nPerms);
+	  for(size_t permIdx = 0; permIdx < nPerms; ++permIdx)
 	    {
-	      break;
+	      fSample[permIdx] = importanceMat[permIdx][featureIdx];
+	      cSample[permIdx] = importanceMat[permIdx][featureIdx + treedata.nFeatures()];
 	    }
-
-	  if(refIcs[featureIdx] == static_cast<size_t>(targetIdx))
-	    {
-	      //cout << refIcs[featureIdx] << " == " << targetIdx << " (" << targetHeader << ")" << endl;
-	      continue;
-	    }
-
-
-	  fprintf(po,"%s\t%s\t%9.8f\t%9.8f\t%9.8f\n",targetName.c_str(),treedata.getFeatureName(refIcs[featureIdx]).c_str(),pValues[featureIdx],importanceValues[featureIdx],treedata.pearsonCorrelation(targetIdx,refIcs[featureIdx]));
-
-	  //os << target_str << "\t" << treedata.get_featureheader(ref_ics[i]) << "\t" 
-	  //   << pvalues[i] << "\t" << ivalues[i] << "\t" << treedata.corr(targetidx,ref_ics[i]) << endl;
+	  datadefs::utest(fSample,cSample,pValues[featureIdx]);
+	  datadefs::mean(fSample,importanceValues[featureIdx],nRealSamples);
 	}
-      //os.close();
-      fclose(po);
-      cout << endl << "Association file created. Format:" << endl;
-      cout << "TARGET   PREDICTOR   P-VALUE   IMPORTANCE   CORRELATION" << endl << endl;
-      cout << "Done." << endl;
+      
+      
+      
+      /////////////////////////////////////////////
+      //  ANALYSIS 2 -- Gradient Boosting Trees  //
+      /////////////////////////////////////////////
+      //bool isGBTEnabled = false; //TODO: EXTEND THE USER PARAMETERS TO INCLUDE THIS
+      if(isGBTEnabled)
+	{
+	  cout << "Gradient Boosting Trees *ENABLED*" << endl;
+	  
+	  //if(!treedata.isFeatureNumerical(targetIdx))
+	  //	{
+	  //	  nTrees *= treedata.nCategories(targetIdx);
+	  //	}
+	  
+	  size_t nMaxLeaves = 6;
+	  num_t shrinkage = 0.5;
+	  num_t subSampleSize = 0.5;
+	  GBT gbt(&treedata, targetIdx, nTrees, nMaxLeaves, shrinkage, subSampleSize);
+	  
+	  
+	  cout <<endl<< "PREDICTION:" << endl;
+	  vector<num_t> prediction(treedata.nSamples());
+	  gbt.predictForest(&treedata, prediction);
+	}
+      else
+	{
+	  cout << "Gradient Boosting Trees *DISABLED*" << endl;
+	}
+      
+      
+      
+      ///////////////////////
+      //  GENERATE OUTPUT  //
+      ///////////////////////  
+      vector<size_t> refIcs(treedata.nFeatures());
+      //vector<string> fnames = treedata.featureheaders();
+      datadefs::sortDataAndMakeRef(pValues,refIcs);
+      datadefs::sortFromRef<num_t>(importanceValues,refIcs);
+      //targetIdx = refIcs[targetIdx];
+      
+      string targetName = treedata.getFeatureName(targetIdx);
+      
+      if(pValues[0] <= pValueThreshold)
+	{
+	  //cout << "Writing associations to file '" << output << "'" << endl;
+	  //ofstream os(output.c_str());
+	  FILE* po;
+	  if(iter == 1)
+	    {
+	      po = fopen(output.c_str(),"w");
+	    }
+	  else
+	    {
+	      po = fopen(output.c_str(),"a");
+	    }
+	  
+	  for(size_t featureIdx = 0; featureIdx < treedata.nFeatures(); ++featureIdx)
+	    {
+	      
+	      if(pValues[featureIdx] > pValueThreshold)
+		{
+		  break;
+		}
+	      
+	      if(refIcs[featureIdx] == static_cast<size_t>(targetIdx))
+		{
+		  //cout << refIcs[featureIdx] << " == " << targetIdx << " (" << targetHeader << ")" << endl;
+		  continue;
+		}
+	      
+	      
+	      fprintf(po,"%s\t%s\t%9.8f\t%9.8f\t%9.8f\n",targetName.c_str(),treedata.getFeatureName(refIcs[featureIdx]).c_str(),pValues[featureIdx],importanceValues[featureIdx],treedata.pearsonCorrelation(targetIdx,refIcs[featureIdx]));
+	      
+	      //os << target_str << "\t" << treedata.get_featureheader(ref_ics[i]) << "\t" 
+	      //   << pvalues[i] << "\t" << ivalues[i] << "\t" << treedata.corr(targetidx,ref_ics[i]) << endl;
+	    }
+	  //os.close();
+	  fclose(po);
+	  cout << endl << "Association file created. Format:" << endl;
+	  cout << "TARGET   PREDICTOR   P-VALUE   IMPORTANCE   CORRELATION" << endl << endl;
+	  cout << "Done." << endl;
+	}
+      else
+	{
+	  cout << endl << "No significant associations found, quitting..." << endl;
+	}
     }
-  else
-    {
-      cout << endl << "No significant associations found, quitting..." << endl;
-    }
-
-  
- 
+      
+      
   return(EXIT_SUCCESS);
 }
