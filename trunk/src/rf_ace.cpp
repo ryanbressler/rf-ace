7463e1863e8b887b5bada664df9c32c62feb8adf
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-07-08 20:04:48 +0000

    Updated version information (0.5.8)

--- trunk/src/rf_ace.cpp
+++ trunk/src/rf_ace.cpp
@@ -35,7 +35,7 @@ void printHeader()
   cout << " --------------------------------------------------------------- " << endl;
   cout << "| RF-ACE -- efficient feature selection with heterogeneous data |" << endl;
   cout << "|                                                               |" << endl;
-  cout << "|  Version:      RF-ACE v0.5.5, July 4th, 2011                  |" << endl;
+  cout << "|  Version:      RF-ACE v0.5.8, July 8th, 2011                  |" << endl;
   cout << "|  Project page: http://code.google.com/p/rf-ace                |" << endl;
   cout << "|  Contact:      timo.p.erkkila@tut.fi                          |" << endl;
   cout << "|                kari.torkkola@gmail.com                        |" << endl;
