9a5d8920a413a10978d515f0c4c7c29358c3a4f9
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-07-15 11:21:59 +0000

    Modified make.bat to include the latest argument parser

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 mkdir bin
-cl /Ox /Febin\rf_ace.exe src\rf_ace.cpp src\getopt_pp.cpp src\randomforest.cpp src\GBT.cpp src\node.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp
+cl /Ox /Febin\rf_ace.exe src\rf_ace.cpp src\argparse.cpp src\randomforest.cpp src\GBT.cpp src\node.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp
 del *.obj
