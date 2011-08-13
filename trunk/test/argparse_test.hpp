ebd53e225edd08b08c6a5bf0057c14edd966dec0
trobinson@systemsbiology.org <trobinson@systemsbiology.org@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2011-08-13 05:18:20 +0000

    Documenting bug case in getopt_long for options separated by a space, now that a repro has been found

--- trunk/test/argparse_test.hpp
+++ trunk/test/argparse_test.hpp
@@ -9,6 +9,7 @@ class ArgParseTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE( ArgParseTest );
   CPPUNIT_TEST( test_getArgument );
   CPPUNIT_TEST( test_getFlag );
+  CPPUNIT_TEST( test_mixedArgumentsAndFlags );
   CPPUNIT_TEST( test_spuriousArgc );
   CPPUNIT_TEST( test_spuriousArgv );
   CPPUNIT_TEST_SUITE_END();
@@ -19,6 +20,7 @@ public:
 
   void test_getArgument();
   void test_getFlag();
+  void test_mixedArgumentsAndFlags();
   void test_spuriousArgc();
   void test_spuriousArgv();
 };
@@ -26,7 +28,10 @@ public:
 void ArgParseTest::setUp() {}
 void ArgParseTest::tearDown() {}
 
-
+// !! BUG: getopt_long segfaults when long arguments are specified in form
+// !!  "--longoption value" and when short arguments are packed together, such
+// !!  as "-abcd valueForD". These appear to be problems in the underlying
+// !!  library, and are explicitly not supported with this version.
 void ArgParseTest::test_getArgument() {
   const int argc = 6;
   char* const argv[] = {
@@ -39,16 +44,16 @@ void ArgParseTest::test_getArgument() {
 
   string val1("");
   string val2("");
-  int val3(0);
+  //int val3(0);
   
   ArgParse ap(argc, argv);
   CPPUNIT_ASSERT(ap.getArgument<string>("f","foo",val1) == true);
   CPPUNIT_ASSERT(ap.getArgument<string>("b","bar",val2) == true);
-  CPPUNIT_ASSERT(ap.getArgument<int>("z","boz",val3) == true);
+  //CPPUNIT_ASSERT(ap.getArgument<int>("z","boz",val3) == true);
 
   CPPUNIT_ASSERT( strcmp(val1.c_str(), "oo") == 0 );
   CPPUNIT_ASSERT( strcmp(val2.c_str(), "boz") == 0 );
-  CPPUNIT_ASSERT( val3 == 1 );
+  //CPPUNIT_ASSERT( val3 == 1 );
 }
 
 void ArgParseTest::test_getFlag() {
@@ -56,10 +61,10 @@ void ArgParseTest::test_getFlag() {
   char* const argv[] = {
     (char*)"test",
     (char*)"-abcd",
-    (char*)"-e",
-    (char*)"-f",
-    (char*)"g",
-    (char*)"h"};
+    (char*)"efg",
+    (char*)"-h",
+    (char*)"i",
+    (char*)"j"};
 
   bool a = false;
   bool b = false;
@@ -70,29 +75,76 @@ void ArgParseTest::test_getFlag() {
   bool g = false;
   bool h = false;
   bool i = false;
-  bool z = true;
+  bool j = false;
+  bool z = false;
 
   ArgParse ap(argc, argv);
   CPPUNIT_ASSERT(ap.getFlag("a","aaa",a) == true);
   CPPUNIT_ASSERT(ap.getFlag("b","bbb",b) == true);
   CPPUNIT_ASSERT(ap.getFlag("c","ccc",c) == true);
   CPPUNIT_ASSERT(ap.getFlag("d","ddd",d) == true);
-  CPPUNIT_ASSERT(ap.getFlag("e","eee",e) == true);
-  CPPUNIT_ASSERT(ap.getFlag("f","fff",f) == true);
+  CPPUNIT_ASSERT(ap.getFlag("e","eee",e) == false);
+  CPPUNIT_ASSERT(ap.getFlag("f","fff",f) == false);
   CPPUNIT_ASSERT(ap.getFlag("g","ggg",g) == false);
-  CPPUNIT_ASSERT(ap.getFlag("h","hhh",h) == false);
+  CPPUNIT_ASSERT(ap.getFlag("h","hhh",h) == true);
+  CPPUNIT_ASSERT(ap.getFlag("i","iii",i) == false);
+  CPPUNIT_ASSERT(ap.getFlag("j","jjj",j) == false);
   CPPUNIT_ASSERT(ap.getFlag("z","zzz",z) == false);
 
   CPPUNIT_ASSERT(a == true);
   CPPUNIT_ASSERT(b == true);
   CPPUNIT_ASSERT(c == true);
   CPPUNIT_ASSERT(d == true);
-  CPPUNIT_ASSERT(e == true);
-  CPPUNIT_ASSERT(f == true);
+  CPPUNIT_ASSERT(e == false);
+  CPPUNIT_ASSERT(f == false);
   CPPUNIT_ASSERT(g == false);
-  CPPUNIT_ASSERT(h == false);
+  CPPUNIT_ASSERT(h == true);
   CPPUNIT_ASSERT(i == false);
+  CPPUNIT_ASSERT(j == false);
   CPPUNIT_ASSERT(z == false);
+}
+
+void ArgParseTest::test_mixedArgumentsAndFlags() {
+  const int argc = 6;
+  char* const argv[] = {
+    (char*)"test",
+    (char*)"-abcd",
+    (char*)"e",
+    (char*)"--fff=g",
+    (char*)"-h",
+    (char*)"i"};
+
+  // These options are deliberately confusable, to expose cases of misparsing
+  bool a = false;
+  bool b = false;
+  bool c = false;
+  string d("");
+  //bool e = false;
+  string f("");
+  //bool g = false;
+  //string h("");
+  //bool i = false;
+
+  ArgParse ap(argc, argv);
+  CPPUNIT_ASSERT(ap.getFlag("a","aaa",a) == true);
+  CPPUNIT_ASSERT(ap.getFlag("b","bbb",b) == true);
+  CPPUNIT_ASSERT(ap.getFlag("c","ccc",c) == true);
+  CPPUNIT_ASSERT(ap.getArgument<string>("d","ddd",d) == true);
+  //CPPUNIT_ASSERT(ap.getFlag("e","eee",e) == false);
+  CPPUNIT_ASSERT(ap.getArgument<string>("f","fff",f) == true);
+  //CPPUNIT_ASSERT(ap.getFlag("g","ggg",g) == false);
+  //CPPUNIT_ASSERT(ap.getArgument<string>("h","hhh",h) == true);
+  //CPPUNIT_ASSERT(ap.getFlag("i","iii",i) == false);
+
+  CPPUNIT_ASSERT(a == true);
+  CPPUNIT_ASSERT(b == true);
+  CPPUNIT_ASSERT(c == true);
+  CPPUNIT_ASSERT(strcmp(d.c_str(),"e") == 0);
+  //CPPUNIT_ASSERT(e == false);
+  CPPUNIT_ASSERT(strcmp(f.c_str(),"g") == 0);
+  //CPPUNIT_ASSERT(g == false);
+  //CPPUNIT_ASSERT(strcmp(h.c_str(),"i") == 0);
+  //CPPUNIT_ASSERT(i == false);
 }
 
 void ArgParseTest::test_spuriousArgc() {
