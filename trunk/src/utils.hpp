cc3a0e622109318d91326de9ededb1a0f91e1d2d
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2012-02-07 13:58:04 +0000

    Restructured the filter program, options, and utils.

--- trunk/src/utils.hpp
+++ trunk/src/utils.hpp
@@ -1,5 +1,8 @@
 #include <vector>
+#include <set>
+#include <string>
 #include "datadefs.hpp"
+#include "treedata.hpp"
 
 using namespace std;
 using datadefs::num_t;
@@ -8,6 +11,10 @@ using datadefs::num_t;
 namespace utils {
 
   vector<num_t> removeNANs(const vector<num_t>& x);
+
+  set<string> readFeatureMask(Treedata& treeData, const string& fileName);
+
+  void pruneFeatures(Treedata& treeData, const string& targetName, const size_t minSamples);
 
 }
 
