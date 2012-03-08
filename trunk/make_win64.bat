1f599d923205a5c37ee890b5fcfd1c6f815df647
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2012-03-08 21:50:32 +0000

    Updated makefile for win64.

--- trunk/make_win64.bat
+++ trunk/make_win64.bat
@@ -1,10 +1,10 @@
 mkdir bin
 
-cl /EHsc /O2 /analyze /Febin\rf-ace-filter-win64.exe src\rf_ace_filter.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\splitter.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\utils.cpp
+cl /EHsc /O2 /analyze /Febin\rf-ace-filter-win64.exe src\rf_ace_filter.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\splitter.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\gamma.cpp src\utils.cpp
 
-cl /EHsc /O2 /analyze /Febin\rf-ace-build-predictor-win64.exe src\rf_ace_build_predictor.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\splitter.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\utils.cpp
+cl /EHsc /O2 /analyze /Febin\rf-ace-build-predictor-win64.exe src\rf_ace_build_predictor.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\splitter.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\gamma.cpp src\utils.cpp
 
-cl /EHsc /O2 /analyze /Febin\rf-ace-predict-win64.exe src\rf_ace_predict.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\splitter.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\utils.cpp
+cl /EHsc /O2 /analyze /Febin\rf-ace-predict-win64.exe src\rf_ace_predict.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\splitter.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\gamma.cpp src\utils.cpp
 
 del *.obj
 
