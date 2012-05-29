8ef15af3d714b3020bbed2b47eca2296d7ccdb8d
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2012-05-29 17:50:12 +0000

    Added the script that creates the package of RF-ACE using "tar" in Linux.

---
+++ trunk/make_package.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+package=$1
+
+tar -czf $package src/*.*pp src/*.h bin/rf-ace* test/*.*pp test/matlab/*.m test/python/*.py test_*by*.tsv test_*by*.arff test_*by*.afm test_predictor.sf testdata.tsv Makefile make_win32.bat make_win64.bat doxy.cfg rf_ace_batch.py rf-ace-launcher.sh 
