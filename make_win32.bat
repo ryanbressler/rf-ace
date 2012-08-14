6844f5ac2cd15d0f87f4ade8a10504843570d278
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2012-08-14 13:54:09 +0000

    Updated Visual Studio make files.

--- make_win32.bat
+++ make_win32.bat
@@ -2,7 +2,7 @@ mkdir bin
 
 SetEnv.cmd /x86 /Release
 
-cl /EHsc /O2 /analyze /Febin\rf-ace-win32.exe src\rf_ace.cpp src\statistics.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\treedata.cpp src\mtrand.cpp src\datadefs.cpp src\math.cpp src\gamma.cpp src\utils.cpp
+cl /EHsc /O2 /analyze /Febin\rf-ace-win32.exe src\rf_ace.cpp src\statistics.cpp src\distributions.cpp src\progress.cpp src\stochasticforest.cpp src\rootnode.cpp src\node.cpp src\treedata.cpp src\datadefs.cpp src\math.cpp src\utils.cpp
 
 del *.obj
 
