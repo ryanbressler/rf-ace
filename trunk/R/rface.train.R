40430f8cda69fba285db57471fa89974cad2a68d
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2012-11-24 07:03:57 +0000

    Feature weights can now be used for training a predictor inside R

--- trunk/R/rface.train.R
+++ trunk/R/rface.train.R
@@ -1,5 +1,8 @@
 rface.train <-
-function(trainData, target, nTrees = 100, mTry = 10, nodeSize = 3, nMaxLeaves = 1000, nThreads = 1) {
-  predictorObj <- .Call("rfaceTrain", trainData, as.character(target), nTrees, mTry, nodeSize, nMaxLeaves, nThreads)
+function(trainData, target, featureWeights = as.vector(rep(1,length(colnames(trainData)))), nTrees = 100, mTry = 10, nodeSize = 3, nMaxLeaves = 1000, nThreads = 1) {
+  #if ( featureWeights == FALSE ) {
+  #  featureWeights = as.vector(list(rep(1,length(colnames(trainData)))))
+  #}
+  predictorObj <- .Call("rfaceTrain", trainData, as.character(target), featureWeights, nTrees, mTry, nodeSize, nMaxLeaves, nThreads)
   return(predictorObj)
 }
