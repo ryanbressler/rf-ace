d0cb2fe70e60fd64dbabfe963c19840f125a9e57
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2012-12-17 05:31:00 +0000

    Prevented another log(0) in calculating entropy.

--- src/treedata.cpp
+++ src/treedata.cpp
@@ -109,7 +109,7 @@ num_t Feature::entropy() const {
 
   for ( ; it != visited_keys.end(); ++it ) {
     num_t f = static_cast<num_t>(it->second) / static_cast<num_t>(nSamples);
-    if ( fabs(f) > datadefs::EPS ) {
+    if ( fabs(f) > datadefs::EPS && fabs(1-f) > datadefs::EPS ) {
       entropy -= f * log(f) + (1-f)*log(1-f);
     }
   }
