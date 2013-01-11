07ef30ab661ed5477671607b8b9ff5a8d55c4593
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2013-01-11 17:41:45 +0000

    Forgot to add the still-empty treedata_newtest.hpp to trunk

---
+++ test/treedata_newtest.hpp
@@ -0,0 +1,22 @@
+#ifndef TREEDATA_NEWTEST_HPP
+#define TREEDATA_NEWTEST_HPP
+
+#include <cstdlib>
+
+#include "newtest.hpp"
+
+using namespace std;
+
+void treedata_newtest_readAFM();
+
+void treedata_newtest() {
+
+  treedata_newtest_readAFM();
+
+}
+
+void treedata_newtest_readAFM() {
+
+}
+
+#endif
