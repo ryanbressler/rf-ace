1ebe26d93b1454acba87965675ef56fcd47bcf68
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2013-01-23 01:27:37 +0000

    Implemented a stochastic greedy splitter for categorical splitters working on numerical targets. Need to account for categorical targets as well...

---
+++ trunk/test/utils_newtest.hpp
@@ -0,0 +1,43 @@
+#ifndef UTILS_NEWTEST_HPP
+#define UTILS_NEWTEST_HPP
+
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <cmath>
+
+#include "datadefs.hpp"
+#include "newtest.hpp"
+
+using namespace std;
+using datadefs::num_t;
+
+
+void utils_newtest_categoricalFeatureSplitsNumericalTarget();
+
+
+void utils_newtest() {
+
+  newtest("Testing categorical feature splitter with numerical targets", &utils_newtest_categoricalFeatureSplitsNumericalTarget);
+
+}
+
+
+void utils_newtest_categoricalFeatureSplitsNumericalTarget() {
+
+  vector<num_t> fv = {1,1,1,2,2,2,3,3,3,4,4,4};
+  vector<num_t> tv = {1,1,1,2,3,4,5,6,7,8,9,10};
+
+  vector<num_t> catOrder = {1,2,3,4};
+
+  map<num_t,vector<size_t> > fmap_left,fmap_right;
+
+  num_t DI = utils::categoricalFeatureSplitsNumericalTarget2(tv,fv,1,catOrder,fmap_left,fmap_right);
+  
+  num_t DI_ref = math::deltaImpurity_regr(math::mean(tv),12,math::mean({1,1,1,2,3,4}),6,math::mean({5,6,7,8,9,10}),6);
+  
+  newassert( fabs( DI - DI_ref ) < datadefs::EPS );
+  
+}
+
+#endif
