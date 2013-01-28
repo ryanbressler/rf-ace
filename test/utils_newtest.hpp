68c1d3e7f36e264309cd4717b83e4c3c74c047b3
timo.erkkila@gmail.com <timo.erkkila@gmail.com@dd2f6f21-4ed5-c9dd-04ba-63aeeebf1885>
2013-01-28 20:04:43 +0000

    Added unit tests for the greedy categorical splitter.

--- test/utils_newtest.hpp
+++ test/utils_newtest.hpp
@@ -8,6 +8,8 @@
 
 #include "datadefs.hpp"
 #include "newtest.hpp"
+#include "utils.hpp"
+#include "math.hpp"
 
 using namespace std;
 using datadefs::num_t;
@@ -48,7 +50,41 @@ void utils_newtest_categoricalFeatureSplitsNumericalTarget() {
 
 void utils_newtest_categoricalFeatureSplitsCategoricalTarget() {
   
+  vector<num_t> fv = {1,1,1,2,2,2,3,3,3,4,4,4};
+  vector<num_t> tv = {1,1,1,2,3,4,5,6,7,8,9,10};
+
+  map<num_t,vector<size_t> > fmap_left,fmap_right;
+
+  num_t DI = utils::categoricalFeatureSplitsCategoricalTarget2(tv,fv,1,{1,2,3,4},fmap_left,fmap_right);
+
+  map<num_t,size_t> freq_left,freq_right,freq_tot;
+  size_t sf_left = 0;
+  size_t sf_right = 0;
+  size_t sf_tot = 0;
+
+  for ( size_t i = 0; i < tv.size(); ++i ) {
+    math::incrementSquaredFrequency(tv[i],freq_tot,sf_tot);
+  }
   
+  for ( size_t i = 0; i < 3; ++i ) {
+    math::incrementSquaredFrequency(tv[i],freq_left,sf_left);
+  }
+
+  for ( size_t i = 3; i < 12; ++i ) {
+    math::incrementSquaredFrequency(tv[i],freq_right,sf_right);
+  }
+  
+  num_t DI_ref = math::deltaImpurity_class(sf_tot,12,sf_left,3,sf_right,9);
+ 
+  newassert( fabs( DI - DI_ref ) < datadefs::EPS );
+
+  fv = {1,1,1,1,1,1,1,1,1,1,1,1};
+
+  DI = utils::categoricalFeatureSplitsNumericalTarget2(tv,fv,1,{1},fmap_left,fmap_right);
+
+  DI_ref = 0;
+
+  newassert( fabs( DI - DI_ref ) < datadefs::EPS );
 
 }
 
